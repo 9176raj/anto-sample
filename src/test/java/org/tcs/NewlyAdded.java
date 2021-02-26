f7e7e17a66144ec9264daeaaeb76d8b5c45219f7
9176raj <9176raj@gmail.com>
2021-02-26 10:55:19 +0530

    checking 1

---
+++ src/test/java/org/tcs/NewlyAdded.java
@@ -0,0 +1,16 @@
+package org.tcs;
+
+public class NewlyAdded {
+
+	private static void tc01() {
+		System.out.println("test completed");
+	}
+	private void tc02() {
+System.out.println();
+	}
+	public static void main(String[] args) {
+		tc01();
+		
+	}
+
+}
